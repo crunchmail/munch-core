d6a0acab84687fb97f5a2fb8c29e5d4e6a2a99d3
Nicolas Brisac <nbrisac@oasiswork.fr>
2017-04-26 23:23:54 +0200

    improved smarthost logging

--- src/munch/apps/transactional/edge.py
+++ src/munch/apps/transactional/edge.py
@@ -17,6 +17,13 @@ log = logging.getLogger(__name__)
 
 
 class EdgeValidators(SmtpValidators):
+
+    def handle_banner(self, reply, address):
+        from time import time
+        self.cid = hex(int(time()*10000000))[2:]
+        log.info('[{}] Incoming connection from {}:{}'.format(
+            self.cid, address[0], address[1]))
+
     def handle_auth(self, reply, creds):
         tls_settings = settings.TRANSACTIONAL.get('SMTP_SMARTHOST_TLS')
         if tls_settings is not None and not self.session.security:
@@ -32,21 +39,29 @@ class EdgeValidators(SmtpValidators):
         if application:
             authenticated = creds.check_secret(application.secret)
             if authenticated:
-                log.info('Successfull login from "{}"'.format(application.username, application.identifier))
+                log.info('[{}] Successfull login from "{}"'.format(
+                    self.cid, application.author.identifier,
+                    application.identifier))
 
         if not authenticated:
             reply.code = '535'
             reply.message = '5.7.8 Authentication credentials invalid'
-            log.warning('Failed login from "{}" (wrong credentials)'.format(
-                creds.authcid))
+            log.warning(
+                '[{}] Failed login from "{}" (invalid credentials)'.format(
+                    self.cid, creds.authcid))
 
     def handle_mail(self, reply, sender, params):
         # Make authentication mandatory
         if not hasattr(self.session, 'auth') or not self.session.auth:
             reply.code = '535'
             reply.message = '5.7.0 Authentification is required'
-            log.warning('Unauthenticated traffic from {} refused'.format(
-                self.session.address[0]))
+            log.warning('[{}] Unauthenticated traffic from {} refused'.format(
+                self.cid, self.session.address[0]))
+
+    def handle_queued(self, reply, results):
+        log.info('[{}] Queued message from <{}> to <{}>'.format(
+            self.cid, self.session.envelope.sender,
+            ', '.join(self.session.envelope.recipients)))
 
 
 class TransactionalSmtpEdge(SmtpEdge):
